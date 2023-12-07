3dade79f23719cf89b93a7b032f9567c493f9c44
Gastón <gastonvalentiniruiz@gmail.com>
2023-12-07 17:34:25 +0100

    Add: Environment variables and settings configured.

--- server/app/app.js
+++ server/app/app.js
@@ -1,4 +1,11 @@
+// Imports
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+
+// Settings
+app.set("PORT", process.env.PORT);
+app.set("DATABASE", process.env.DATABASE);
 
 export { app };
