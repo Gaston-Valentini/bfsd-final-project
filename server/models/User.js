84324418bd373b570bbc5f52d5ec245af7966a83
Gastón <gastonvalentiniruiz@gmail.com>
2023-12-11 13:10:52 +0100

    Add: User model defined.

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const User = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    surname: {
+        type: String,
+        required: true,
+    },
+    nickname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: Number,
+    birthday: Date,
+    sport: String,
+    image: String,
+    ubication: String,
+    biography: {
+        type: String,
+        maxlength: 500,
+    },
+});
+
+export default model("User", User);
