452cfdca64b8dadc19080648fbef79cc2ee3aea6
Gastón <gastonvalentiniruiz@gmail.com>
2023-12-11 13:17:55 +0100

    Add: Backend middlewares configured.

--- server/app/app.js
+++ server/app/app.js
@@ -3,9 +3,18 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 
 // Settings
 app.set("PORT", process.env.PORT);
 app.set("DATABASE", process.env.DATABASE);
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
 
 export { app };
